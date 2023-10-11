65ed66d3d3e5608046c63ecb40cd2cc295da91a3
ssm8892 <ssm48892@gmail.com>
2023-10-11 15:24:44 -0500

    successful open ai request

--- app.js
+++ app.js
@@ -1,35 +1,115 @@
-const express = require('express');
+/*const express = require('express');
 const { exec } = require('child_process');
+const bodyParser = require('body-parser');
 const path = require('path');
+const OpenAIApi = require('openai');
+const fs = require('fs');
 const hbs = require('hbs');
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
+*/
+import express from 'express';
+import { exec } from 'child_process';
+import bodyParser from 'body-parser';
+import path from 'path';
+import OpenAI from 'openai';
+import fs from 'fs';
+import hbs from 'hbs';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(process.cwd(), 'views'));
+
+const openai = new OpenAI({
+    apiKey: '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'
+});
 
 app.get('/styles.css', (req, res) => {
     res.setHeader('Content-Type', 'text/css');
-    res.sendFile(__dirname + '/styles.cssnp');
+    res.sendFile( path.join(process.cwd()) + '/styles.css');
 });
 
 app.get('/', (req, res) => {
     res.render('login');
 });
 
-app.get('/runscript/:userEmail', (req, res) => {
-    const userEmail = req.params.userEmail;
+app.post('/runscript', (req, res) => {
+    const userEmail = req.body.email;
 
-    exec(`python path_to_script.py ${userEmail}`, (error, stdout, stderr) => {
+    exec(`python isolate_data.py ${userEmail}`, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             return res.status(500).send('Error executing the script.');
         }
-        res.send(`Script output: ${stdout}`);
+        console.log("python file has executed, now redirecting to analyze");
+        res.redirect('/analyze');
     });
+});
+
+app.get('/insights', (req, res) => {
+    res.render('insights');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
 });
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+app.get('/analyze', async (req, res) => {
+    console.log("analyze is being called");
+    const userData = JSON.parse(fs.readFileSync('py_output/user_data.json', 'utf8'));
+    const userRoleData = JSON.parse(fs.readFileSync('py_output/user_role_data.json', 'utf8'));
+    const userLocationData = JSON.parse(fs.readFileSync('py_output/user_location_data.json', 'utf8'));
+
+    console.log("json files are read inside analyze route");
+    // Construct your prompt using the data
+    const prompt = `
+    Analyze the property insights ${JSON.stringify(userLocationData)},
+    dashboards that the user frequently visits ${JSON.stringify(userData["Dashboard"])},
+    criticality of the property insights ${JSON.stringify(userLocationData["Criticality"])},
+    and whether the user's role's responsibilities match the insights ${JSON.stringify(userRoleData["Duties"])}.
+    Then generate the top 5 insights in meaningful sentences for the user to read 
+    along with the info about which property the insight is referring to 
+    and the criticality of the property in a JSON format.
+      `;
+    console.log("prompt is generated");
+    const openaiResponse = await openai.completions.create({
+        model: 'gpt-3.5-turbo-instruct',
+        prompt: prompt,
+        max_tokens: 1000,
+    });
+    console.log("prompt is sent to openai and came back with response");
+    console.log(openaiResponse);
+    fs.writeFileSync('py_output/openai_response.json', JSON.stringify(openaiResponse.data.choices[0].text.trim()));
+
+    res.render('insights', { insights: openaiResponse.data.choices[0].text.trim() });
+
+});
+//narani ramesh - cbre
+
+
+/*Given a user with the following data:
+${JSON.stringify(userData)}
+Role and responsibilities:
+${JSON.stringify(userRoleData)}
+Properties and criticality:
+${JSON.stringify(userLocationData)}
+Analyze the property insights, dashboards that the user frequently visits, criticality of the property insights, 
+and whether the user's role's resposibilities match the insights.
+Then generate the top 5 insights in meaningful sentences for the user to read along with the info about the which property 
+the insight is referring to and the criticality of the property in a JSON format.
+*/
+
--- isolate_data.py
+++ isolate_data.py
@@ -1,21 +1,16 @@
 import pandas as pd
 import sys
 import json
+import os
 
 def get_isolated_data(user_email, set1_path, set2_path, set3_path):
-    # Load the datasets
     set1 = pd.read_csv(set1_path)
     set2 = pd.read_csv(set2_path)
     set3 = pd.read_csv(set3_path)
 
-    # Extracting user's data from set2
     user_data = set2[set2['Email'] == user_email]
-
-    # Extracting insights related to user's role from set1
     user_role_data = set1[set1['Role'] == user_data['Role'].iloc[0]]
-
-    # Extracting properties in user's location from set3
-    user_location_data = set3[set3['Address'] == user_data['Location'].iloc[0]]
+    user_location_data = set3[set3['Property Address'].str.contains(user_data['Location'].iloc[0]) & set3['Account'].str.contains(user_data['Client'].iloc[0])]
 
     return user_data, user_role_data, user_location_data
 
@@ -26,17 +21,20 @@ if __name__ == "__main__":
 
     user_email = sys.argv[1]
     
-    # Paths to the CSV datasets
-    set1_path = 'path_to_set1.csv'
-    set2_path = 'path_to_set2.csv'
-    set3_path = 'path_to_set3.csv'
+    set1_path = './input_files/roles_duties_and_insights.csv'
+    set2_path = './input_files/recommendation_engine_users.csv'
+    set3_path = './input_files/property_insights_extended.csv'
 
     user_data, user_role_data, user_location_data = get_isolated_data(user_email, set1_path, set2_path, set3_path)
     
-    # Saving the isolated data to JSON files
-    with open('user_data.json', 'w') as f:
+    # Ensure py_output directory exists
+    if not os.path.exists('py_output'):
+        os.makedirs('py_output')
+
+    # Write to JSON files in py_output directory
+    with open('py_output/user_data.json', 'w') as f:
         json.dump(user_data.to_dict(orient='records'), f)
-    with open('user_role_data.json', 'w') as f:
+    with open('py_output/user_role_data.json', 'w') as f:
         json.dump(user_role_data.to_dict(orient='records'), f)
-    with open('user_location_data.json', 'w') as f:
+    with open('py_output/user_location_data.json', 'w') as f:
         json.dump(user_location_data.to_dict(orient='records'), f)
