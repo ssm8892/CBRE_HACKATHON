6c8de69b599a69176ba699c32cf4d88d4bdf30d9
ssm8892 <ssm48892@gmail.com>
2023-10-12 09:55:00 -0500

    final commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ import express from 'express';
 import { exec } from 'child_process';
 import bodyParser from 'body-parser';
 import path from 'path';
-import OpenAI from 'openai';
+//import OpenAI from 'openai';
 import fs from 'fs';
 import hbs from 'hbs';
 
@@ -30,13 +30,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(process.cwd(), 'views'));
 
+/*
 const openai = new OpenAI({
-    apiKey: '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'
+    apiKey: 
 });
+*/
 
 app.get('/styles.css', (req, res) => {
     res.setHeader('Content-Type', 'text/css');
-    res.sendFile( path.join(process.cwd()) + '/styles.css');
+    res.sendFile(path.join(process.cwd()) + '/styles.css');
 });
 
 app.get('/', (req, res) => {
@@ -51,8 +53,25 @@ app.post('/runscript', (req, res) => {
             console.error(`exec error: ${error}`);
             return res.status(500).send('Error executing the script.');
         }
-        console.log("python file has executed, now redirecting to analyze");
-        res.redirect('/analyze');
+        fs.readFile('./py_output/script_output.json', 'utf8', (err, jsonString) => {
+            if (err) {
+                console.error('Error reading the file', err);
+                return res.status(500).send('Failed to read JSON data.');
+            }
+
+            // Parse the JSON data
+            const data = JSON.parse(jsonString);
+
+            // Convert each object to a dictionary and add the index field
+            for (let i = 0; i < data.length; i++) {
+                data[i]["index"] = i + 1; // 1-based index
+            }
+
+            // Redirect to another route and pass the processed data as parameters
+            // For simplicity, redirecting to '/next-route' with the first item's property address as an example.
+            // Adjust as needed.
+            res.render('insights', { insights: data});
+        });
     });
 });
 
@@ -68,6 +87,7 @@ app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
 
+/*
 app.get('/analyze', async (req, res) => {
     console.log("analyze is being called");
     const userData = JSON.parse(fs.readFileSync('py_output/user_data.json', 'utf8'));
@@ -83,33 +103,57 @@ app.get('/analyze', async (req, res) => {
     and whether the user's role's responsibilities match the insights ${JSON.stringify(userRoleData["Duties"])}.
     Then generate the top 5 insights in meaningful sentences for the user to read 
     along with the info about which property the insight is referring to 
-    and the criticality of the property in a JSON format.
+    and the criticality of the property in a dictionary format with the following keys address, insight_statement, dashboard, and client_name.
       `;
     console.log("prompt is generated");
     const openaiResponse = await openai.completions.create({
         model: 'gpt-3.5-turbo-instruct',
         prompt: prompt,
-        max_tokens: 1000,
+        max_tokens: 2797,
     });
     console.log("prompt is sent to openai and came back with response");
     console.log(openaiResponse);
-    fs.writeFileSync('py_output/openai_response.json', JSON.stringify(openaiResponse.data.choices[0].text.trim()));
+    const jsonResponse = JSON.stringify(openaiResponse.choices[0].text.trim());
+    fs.writeFileSync('AI_output_1.json', jsonResponse);
 
-    res.render('insights', { insights: openaiResponse.data.choices[0].text.trim() });
+    res.render('insights');
 
 });
-//narani ramesh - cbre
+*/
+
+/*
+Analyze the property insights ${JSON.stringify(userLocationData)},
+    dashboards that the user frequently visits ${JSON.stringify(userData["Dashboard"])},
+    criticality of the property insights ${JSON.stringify(userLocationData["Criticality"])},
+    and whether the user's role's responsibilities match the insights ${JSON.stringify(userRoleData["Duties"])}.
+    Then generate the top 5 insights in meaningful sentences for the user to read 
+    along with the info about which property the insight is referring to 
+    and the criticality of the property in a JSON format.
 
 
-/*Given a user with the following data:
-${JSON.stringify(userData)}
-Role and responsibilities:
-${JSON.stringify(userRoleData)}
-Properties and criticality:
-${JSON.stringify(userLocationData)}
-Analyze the property insights, dashboards that the user frequently visits, criticality of the property insights, 
-and whether the user's role's resposibilities match the insights.
-Then generate the top 5 insights in meaningful sentences for the user to read along with the info about the which property 
-the insight is referring to and the criticality of the property in a JSON format.
+    Drop the row if the insight category for that row is not mentioned in field names from ${JSON.stringify(userData["Dashboard"])}.
+
+    Then rerank the insights by adding a bit more weight to higher criticalities, where critical is the highest valued. The output should be 
+        in JSON file containing all the insights, and each insights should have the following fields in this order, "Rank", "Criticality_Level", 
+        "Insight_Category", "Address", "Insight_Statements", "Client". For the statements, expand beyond driver provided 
+        and write a longer statement while considering what the user's responsibilities in ${JSON.stringify(userRoleData)}. 
+        Keep it under 10 to 15 words.
+
+
+        Output the final json file after processing the entire prompt.
+
+        Based on the Insight 1, Insight 2, and the Driver from ${JSON.stringify(userLocationData)} categorize each row from this file into a specific dashboard from one of these 
+        dashboards: Asset Performance, Asset Valuation, Building Health, Client Accounts, Energy Usage, Equipment Health, Equipment Monitoring, Facility Utilization, 
+        Investment Returns, Lease Expirations, Maintenance Schedules, New Leases, Occupancy Rate, Preventative Maintenance, Rent Collection, 
+        Revenue Metrics, Work Order Completion, Work Order Status. 
+
+        Arrange the remaining JSON objects
+        in a JSON file, and each insights should have the following fields in this order, "Rank", "Criticality_Level", 
+        "Insight_Category", "Address", "Insight_Statements", "Client". For the statements, expand beyond driver provided 
+        and write a longer statement while considering what the user's responsibilities in ${JSON.stringify(userRoleData)}. 
+        Keep it under 10 to 15 words.
+
+        Only output the resulting JSON file with the rows in your response.
+        Your reponse should be able to parsed by JSON.
 */
 
--- isolate_data.py
+++ isolate_data.py
@@ -2,17 +2,60 @@ import pandas as pd
 import sys
 import json
 import os
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 def get_isolated_data(user_email, set1_path, set2_path, set3_path):
-    set1 = pd.read_csv(set1_path)
-    set2 = pd.read_csv(set2_path)
-    set3 = pd.read_csv(set3_path)
+    df_user = pd.read_csv(set2_path)
+    df_location = pd.read_csv(set3_path)
+    df_role = pd.read_csv(set1_path)
+    
+    df_user = df_user[df_user['Email']==user_email]
+    df_location = df_location[df_location['Property Address'].str.contains(df_user['Location'].iloc[0]) & df_location['Account'].str.contains(df_user['Client'].iloc[0])]
 
-    user_data = set2[set2['Email'] == user_email]
-    user_role_data = set1[set1['Role'] == user_data['Role'].iloc[0]]
-    user_location_data = set3[set3['Property Address'].str.contains(user_data['Location'].iloc[0]) & set3['Account'].str.contains(user_data['Client'].iloc[0])]
+    dashboards = ["Asset Performance", "Asset Valuation", "Building Health", "Client Accounts", "Energy Usage", "Equipment Health", "Equipment Monitoring", "Facility Utilization", 
+        "Investment Returns", "Lease Expirations", "Maintenance Schedules", "New Leases", "Occupancy Rate", "Preventative Maintenance", "Rent Collection", 
+        "Revenue Metrics", "Work Order Completion", "Work Order Status"]
+    criticality_rank = {"Critical": 3, "High": 2, "Medium": 1, "Low": 0}
 
-    return user_data, user_role_data, user_location_data
+    relevant_dashboards = df_user["Dashboard"].unique().tolist()
+    user_duties = df_role[df_role["Role"] == df_user["Role"].iloc[0]]["Duties"].str.split(', ').iloc[0]
+
+    df_location["Dashboard"] = df_location.apply(lambda row: get_most_similar_dashboard(row["Insight 1"] + " " + row["Insight 2"] + " " + row["Driver"], dashboards), axis=1)
+
+    relevant_properties = df_location[df_location["Account"] == df_user["Client"].iloc[0]]
+
+    def calculate_score(row):
+        user_activity_series = df_user[df_user["Dashboard"] == row["Dashboard"]]["User Activity"]
+    # If the property's dashboard is relevant to the user
+        if not user_activity_series.empty:
+            activity_score = user_activity_series.mean()  # Using mean() to account for multiple entries
+            return activity_score + criticality_rank[row["Criticality"]]
+    # If not, score based on its relevance to the user's duties
+        else:
+            most_similar_duty = get_most_similar_duty(row["Insight 1"] + " " + row["Insight 2"] + " " + row["Driver"], user_duties)
+            similarity_score = cosine_similarity(TfidfVectorizer().fit_transform([most_similar_duty, row["Insight 1"] + " " + row["Insight 2"] + " " + row["Driver"]]))[0, 1]
+        return similarity_score + criticality_rank[row["Criticality"]]
+
+    relevant_properties["Score"] = relevant_properties.apply(calculate_score, axis=1)
+    sorted_properties = relevant_properties.sort_values(by="Score", ascending=False)
+    sorted_properties = sorted_properties.head(5)
+
+    sorted_properties = sorted_properties.rename(columns={'Property Address': 'Prop_Address', 'Insight 1': 'Ins1', 'Insight 2': 'Ins2'})
+    return sorted_properties
+
+def get_most_similar_dashboard(insight, dashboards):
+    vectorizer = TfidfVectorizer().fit(dashboards)
+    vectors = vectorizer.transform([insight] + dashboards)
+    cosine_similarities = cosine_similarity(vectors[0:1], vectors).flatten()
+    return dashboards[cosine_similarities[1:].argmax()]
+
+def get_most_similar_duty(insight, duties):
+    vectorizer = TfidfVectorizer().fit(duties)
+    vectors = vectorizer.transform([insight] + duties)
+    cosine_similarities = cosine_similarity(vectors[0:1], vectors).flatten()
+    return duties[cosine_similarities[1:].argmax()]
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -25,16 +68,12 @@ if __name__ == "__main__":
     set2_path = './input_files/recommendation_engine_users.csv'
     set3_path = './input_files/property_insights_extended.csv'
 
-    user_data, user_role_data, user_location_data = get_isolated_data(user_email, set1_path, set2_path, set3_path)
+    user_location_data = get_isolated_data(user_email, set1_path, set2_path, set3_path)
     
     # Ensure py_output directory exists
     if not os.path.exists('py_output'):
         os.makedirs('py_output')
 
-    # Write to JSON files in py_output directory
-    with open('py_output/user_data.json', 'w') as f:
-        json.dump(user_data.to_dict(orient='records'), f)
-    with open('py_output/user_role_data.json', 'w') as f:
-        json.dump(user_role_data.to_dict(orient='records'), f)
-    with open('py_output/user_location_data.json', 'w') as f:
+    # Write to JSON file in py_output directory
+    with open('py_output/script_output.json', 'w') as f:
         json.dump(user_location_data.to_dict(orient='records'), f)
